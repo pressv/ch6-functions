bf9a5ca693cf2e2b5b6a6809c34b2ff3c3914fc6
Ott Toomet <otoomet@gmail.com>
2017-09-27 06:31:17 -0700

    ex1/ex1.R: mentioned option to convert from feet to meters

--- exercise-1/exercise.R
+++ exercise-1/exercise.R
@@ -1,15 +1,18 @@
-# Exercise 1: writing and executing functions
+## Exercise 1: writing and executing functions
 
-# Write a function `AddThree` that adds 3 to an input value
-
-
-# Create a variable `ten` by passing 7 to your `AddThree` function
+## Write a function `AddThree` that adds 3 to an input value
 
 
-# Write a function `FeetToMeters` that converts from feet to meters
+## Create a variable `ten` by passing 7 to your `AddThree` function
 
 
-# Create a variable `height.in.feet` that is your height in feet
+## Write a function `FeetToMeters` that converts from feet to meters
+## 
+## Note: if you come with metric background, you may want to do the following three questions
+## in the opposite way: create function 'metersToFeet' and compute you height in feet below
 
 
-# Create a variable `height.in.meters` by passing `height.in.feet` to your `FeetToMeters` function
+## Create a variable `height.in.feet` that is your height in feet
+
+
+## Create a variable `height.in.meters` by passing `height.in.feet` to your `FeetToMeters` function
